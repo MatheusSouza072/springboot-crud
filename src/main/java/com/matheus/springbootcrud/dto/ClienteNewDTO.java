87d294d6552e01334d5d7bd1492a4b1a59383dd1
Matheuskbrasil <matheus.070202@gmail.com>
2019-09-03 13:43:31 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/matheus/springbootcrud/dto/ClienteNewDTO.java
+++ src/main/java/com/matheus/springbootcrud/dto/ClienteNewDTO.java
@@ -11,7 +11,7 @@ import com.matheus.springbootcrud.services.validation.ClienteInsert;
 @ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	 
 	@NotEmpty(message="Preenchimento obrigatório")
 	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
